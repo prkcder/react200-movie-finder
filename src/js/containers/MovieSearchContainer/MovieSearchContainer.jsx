ac190bd1083af538100fa043bcdf186ab70a4401
John Park <jhnprk12@gmail.com>
2019-07-11 20:31:59 -0700

    initial commit

---
+++ src/js/containers/MovieSearchContainer/MovieSearchContainer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class MovieSearchContainer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Movie Search Container</h1>
+      </div>
+    )
+  }
+}
+
+// export default MovieSearchContainer;
--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from './reducers';
+import { createStore, applyMiddleware, compose } from 'redux';
+import promise from 'redux-promise-middleware';
 import App from './app';
+// import reducers from './reducers'; --> will be reducer from search
 
-/* eslint-disable no-underscore-dangle */
-const store = createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-/* eslint-enable */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+// update to include search reducer createStore(searchReducer, composeEnhancers...)
+const store = createStore(composeEnhancers(applyMiddleware(promise)));
 
+/* Update render method invoke */
 render(
   <Provider store={ store }>
     <App />
